e0457e384c331238255eb8723e30c6c0402861b1
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-18 17:03:15 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,4 +16,5 @@ ActiveRecord::Schema.define(version: 1) do
     t.string "coupon_code"
     t.string "store"
   end
+
 end
